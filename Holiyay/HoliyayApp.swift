3c1c986a494c364054132ab9e5a1befdda1be442
Lapluss Darkness <yujioshinozaki@gmail.com>
2022-05-24 07:52:11 +0700

    Initial Commit

---
+++ Holiyay/HoliyayApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoliyayApp.swift
+//  Holiyay
+//
+//  Created by MacBook on 24/05/22.
+//
+
+import SwiftUI
+
+@main
+struct HoliyayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
